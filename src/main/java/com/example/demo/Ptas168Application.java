aad478071ec967d9d937661b1da8edec411c3768
V2 <vitou@V2s-Mac-mini.local>
2026-01-13 17:02:29 +0700

    init

---
+++ src/main/java/com/example/demo/Ptas168Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ptas168Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ptas168Application.class, args);
+	}
+
+}
